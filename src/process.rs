5c17cb82db794ce315c74482af6c645f6a3540ce
Sajox <49364438+sajox@users.noreply.github.com>
2024-06-01 22:30:11 +0200

    Updated replay_trace again :)

--- src/process.rs
+++ src/process.rs
@@ -177,13 +177,17 @@ impl Process {
     }
 
     /// Replay a trace from a process run. It will be sequential. Only Tasks and gateways is traced that might mutate data.
-    pub fn replay_trace<T>(handler: &Eventhandler<T>, data: T, trace: &[(&str, String)]) -> T
+    pub fn replay_trace<T>(
+        handler: &Eventhandler<T>,
+        data: T,
+        trace: &[(impl AsRef<str>, impl AsRef<str>)],
+    ) -> T
     where
         T: std::fmt::Debug,
     {
         let data = Arc::new(Mutex::new(data));
-        for (ty, id) in trace {
-            match *ty {
+        for (ty, id) in trace.iter().map(|(ty, id)| (ty.as_ref(), id.as_ref())) {
+            match ty {
                 TASK => {
                     let _ = handler.run_task(id, Arc::clone(&data));
                 }
